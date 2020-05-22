af8a6b3db17fcf2835f13e039df1db2e7c8534f3
Jorge Eliecer <jorge.m.h@outlook.com>
2020-05-21 22:10:07 -0500

    methods on Python

--- Python/main.py
+++ Python/main.py
@@ -1,8 +1,8 @@
 from Python.car import Car
+from Python.account import Account
 
 if __name__ == "__main___":
     print("Hola mundo")
-    car = Car()
-    car.license = "AMS345"
-    car.driver = "Jorge Herrera"
-    print(vars(car))
+    
+    car = Car("AMS345", Account("Jorge Herrera", "91038910"))
+    print(vars(car.driver))
