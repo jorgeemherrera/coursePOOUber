a2763e74f42029666e27463a1c34cf7b5d877551
Jorge Eliecer <jorge.m.h@outlook.com>
2020-05-21 21:36:57 -0500

    methods on Python

--- Python/main.py
+++ Python/main.py
@@ -1,2 +1,8 @@
+from Python.car import Car
+
 if __name__ == "__main___":
     print("Hola mundo")
+    car = Car()
+    car.license = "AMS345"
+    car.driver = "Jorge Herrera"
+    print(vars(car))
