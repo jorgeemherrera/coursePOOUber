027c08cc1e0c54e37f5572dce9763fadce444bac
Jorge Eliecer <jorge.m.h@outlook.com>
2020-05-23 16:12:11 -0500

    inheritance in PHP from Account

---
+++ PHP/user.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once("account.php");
+
+class User extends Account {
+  public function __construct($name, $document, $email, $password) {
+    parent::__construct($name, $document);
+    $this->email = $email;
+    $this->password = $password;
+  }
+}
+
+?>
