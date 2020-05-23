5b96a7ec92f1cc1c03dfa3e7e86369368f3ffc39
Jorge Eliecer <jorge.m.h@outlook.com>
2020-05-23 15:58:51 -0500

    create objects in JS

--- JS/index.js
+++ JS/index.js
@@ -1,3 +1,12 @@
 let car = new Car("AWR455", new Account("Andres Herrera", "1020400090"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX(
+  "TYU678",
+  new Account("Andrea Ferran", "AND890",),
+  "Chevrolet",
+  "Spark"
+);
+uberX.passenger = 4;
+uberX.printDataCar();
